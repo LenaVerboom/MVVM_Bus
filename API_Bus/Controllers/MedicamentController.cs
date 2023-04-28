b40be64f104c4e85772bbce9b5ea8792b112803d
Lena VERBOOM <lena.verboom@gmail.com>
2023-04-28 14:39:42 +0200

    api fonctionne ouuhhouuhhh

--- API_Bus/Controllers/MedicamentController.cs
+++ API_Bus/Controllers/MedicamentController.cs
@@ -1,11 +1,7 @@
 ﻿
 using API_Bus.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace API_Bus.Controllers
 {
@@ -37,12 +33,65 @@ namespace API_Bus.Controllers
             {
                 return NotFound();
             }
-            var artist = await _databaseContext.Medicaments.FindAsync(id);
-            if (artist == null)
+            var medicament = await _databaseContext.Medicaments.FindAsync(id);
+            if (medicament == null)
             {
                 return NotFound();
             }
-            return artist;
+            return medicament;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Medicament>> DeleteMedicament(int id)
+        {
+            if (_databaseContext.Medicaments == null)
+            {
+                return NotFound();
+            }
+            var medicament = await _databaseContext.Medicaments.FindAsync(id);
+            if (medicament == null)
+            {
+                return NotFound();
+            }
+            _databaseContext.Medicaments.Remove(medicament);
+            await _databaseContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Medicament>> PostMedicament(Medicament medoc)
+        {
+            if (_databaseContext.Medicaments == null)
+            {
+                return NotFound();
+            }
+            
+           
+            _databaseContext.Medicaments.Add(medoc);
+            await _databaseContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetMedicament),new {id = medoc.MedocId},medoc) ;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedicament(int id, Medicament medoc)
+        {
+            if (id != medoc.MedocId)
+            {
+                return BadRequest();
+            }
+            // update database
+            _databaseContext.Entry(medoc).State = EntityState.Modified;
+
+            try
+            {
+                await _databaseContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                
+            }
+            return Ok();
+
         }
     }
     
